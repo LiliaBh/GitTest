abc46ad34f84d256695643c005056338a045f0df
LiliaBh <lili.hamouda@gmail.com>
2017-04-20 15:25:28 +0200

    Added a few Stuff

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -7,7 +7,7 @@ public class JavaApplication2 {
     public static void main(String[] args) {
        
     System.out.println("Soso lala");
-    Scanner s = new Scanner(System.in);
+    Scanner s = new Scanner(System.in);//Hey Hey
     String Name= s.nextLine();
     System.out.println(Name);
     
