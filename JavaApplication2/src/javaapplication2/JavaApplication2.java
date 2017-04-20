a9c07bf28cfe9ced97fb2a94931b2a5cd539f302
LiliaBh <lili.hamouda@gmail.com>
2017-04-20 15:25:00 +0200

    Added a few Stuff

---
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,17 @@
+
+package javaapplication2;
+import java.util.*;
+
+public class JavaApplication2 {
+
+    public static void main(String[] args) {
+       
+    System.out.println("Soso lala");
+    Scanner s = new Scanner(System.in);
+    String Name= s.nextLine();
+    System.out.println(Name);
+    
+    
+    }
+    
+}
